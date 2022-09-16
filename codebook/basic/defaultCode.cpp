8a01da7e5db81fff48711a181c62db5678151fa6
jteng2127 <jteng2127@gmail.com>
2022-09-16 17:11:26 +0800

    add default code

---
+++ codebook/basic/defaultCode.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define MXN (int)(1e5+5)
+#define MOD (int)(1e9+7)
+using namespace std;
+
+ll n, m, k;
+string s;
+
+void solve(){}
+
+int main(){
+  int t=1;
+  cin >> t;
+  while(t--){
+    solve(); } }
