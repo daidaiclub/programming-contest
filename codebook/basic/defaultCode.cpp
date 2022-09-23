184d9d03f152603e226c5a3f372bba1403fc37d2
jteng2127 <jteng2127@gmail.com>
2022-09-23 11:49:51 +0800

    modify default code

--- codebook/basic/defaultCode.cpp
+++ codebook/basic/defaultCode.cpp
@@ -1,9 +1,14 @@
 #include<bits/stdc++.h>
 #define ll long long
 #define ld long double
-#define MXN (int)(1e5+5)
-#define MOD (int)(1e9+7)
+#define INF 0x3f3f3f3f
+#define LLINF 0x3f3f3f3f3f3f3f3f
+#define X first
+#define Y second
 using namespace std;
+
+const int MXN = 1e5+5;
+const int MOD = 1e9+7;
 
 ll n, m, k;
 string s;
