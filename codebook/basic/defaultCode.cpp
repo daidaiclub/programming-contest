a3e0448cb3430a3380b9c8a41e911ad70857fb1c
jteng2127 <jteng2127@gmail.com>
2022-09-26 22:29:56 +0800

    update default code

--- codebook/basic/defaultCode.cpp
+++ codebook/basic/defaultCode.cpp
@@ -5,18 +5,14 @@
 #define LLINF 0x3f3f3f3f3f3f3f3f
 #define X first
 #define Y second
+#define PB emplace_back
+#define pll pair<long, long>
 using namespace std;
+const int MXN = 4e5+5;
 
-const int MXN = 1e5+5;
-const int MOD = 1e9+7;
-
-ll n, m, k;
-string s;
-
-void solve(){}
-
+void sol(){}
 int main(){
   int t=1;
   cin >> t;
   while(t--){
-    solve(); } }
+    sol(); } }
