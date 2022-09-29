9bb632d93bcfce9b3bc038a24266847531f3c078
jteng2127 <jteng2127@gmail.com>
2022-09-29 23:24:37 +0800

    update default code

--- codebook/basic/defaultCode.cpp
+++ codebook/basic/defaultCode.cpp
@@ -2,13 +2,14 @@
 #define ll long long
 #define ld long double
 #define INF 0x3f3f3f3f
-#define NINF 0xc1c1c1c1
 #define LLINF 0x3f3f3f3f3f3f3f3f
+#define NINF 0xc1c1c1c1
 #define NLLINF 0xc1c1c1c1c1c1c1c1
 #define X first
 #define Y second
 #define PB emplace_back
 #define pll pair<long, long>
+#define MEM(a,n) memset(a, n, sizeof(a))
 using namespace std;
 const int MXN = 4e5+5;
 
