55b35ff6d84027572e49d2eb9b005ae83c63db44
jteng2127 <jteng2127@gmail.com>
2022-10-08 00:04:31 +0800

    compress BIT.cpp

--- codebook/dataStructure/BIT.cpp
+++ codebook/dataStructure/BIT.cpp
@@ -4,23 +4,18 @@ struct BIT{
   ll a[MXN];
   void init(int _n){
     n = _n;
-    MEM(a, 0);
-  }
+    MEM(a, 0); }
   void add(int i, int x){
     for(; i <= n; i += i & -i)
-      a[i] += x;
-  }
+      a[i] += x; }
   int sum(int i){
     int ret = 0;
     for(; i > 0; i -= i & -i)
       ret += a[i];
-    return ret;
-  }
+    return ret; }
   int kth(int k){
     int res = 0;
     for(int i = 1 << __lg(n); i > 0; i >>= 1)
       if(res + i <= n && a[res + i] < k)
         k -= a[res += i];
-    return res;
-  }
-};
+    return res; } };
