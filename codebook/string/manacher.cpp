8fb261376d45eef936b0d7d185caa3b4433d2a6c
jteng2127 <jteng2127@gmail.com>
2022-11-07 00:38:55 +0800

    modify manacher

--- codebook/string/manacher.cpp
+++ codebook/string/manacher.cpp
@@ -1,22 +1,17 @@
-struct Manacher {
+const int MXN = 1e7 + 5;
+struct Manacher { // 0-base 每個點為中心的最長回文長度
   char str[MXN]; int p[MXN], len = 0;
-  void init(string s) {
-    MEM(p, 0);
-    str[len++] = '$', str[len++] = '#';
-    int sz = s.size();
-    for(int i = 0; i < sz; ++i)
+  void init(string s) { // O(n)
+    MEM(p, 0); str[len++] = '$', str[len++] = '#';
+    for(int i = 0; i < s.size(); ++i)
       str[len++] = s[i], str[len++] = '#';
     str[len] = '*';
     int mx = 0, id = 0;
     for(int i = 1; i < len; ++i) {
       p[i] = mx > i ? min(p[(id<<1) - i], mx - i) : 1;
       while(str[i + p[i]] == str[i - p[i]]) p[i]++;
-      if(i + p[i] > mx) {
-        mx = i + p[i];
-        id = i;}}}
-  int query(int l, int r) {
-    int ans = 0;
-    l = 2 * l + 2, r = 2 * r + 2;
-    for(int i = l; i < r; i++)
-      ans = max(ans, p[i]);
-    return ans - 1;}};
+      if(i + p[i] > mx)
+        mx = i + p[i], id = i;
+    } } // bt=1: middle between mid, mid+1
+  int query(int mid, bool bt = 0){
+    return p[mid * 2 + 2 + bt] - 1; } };
